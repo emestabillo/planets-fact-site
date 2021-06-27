31a7c9dbe6bef21d342fb26ed0d08c0920756b1c
Emmilie Estabillo <emsestabillo@gmail.com>
2021-06-27 14:10:10 -0400

    Move stars background to public folder

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7f4c2684bc56f2efa42a"; }
+/******/ 		__webpack_require__.h = function() { return "8e2315c36eeae405c294"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
-const path = require("path");
 const withImages = require("next-images");
-module.exports = withImages({
-  exclude: path.resolve(__dirname, "src/assets/svg"),
-  webpack(config, options) {
-    return config;
-  },
-});
+module.exports = withImages();
 
 module.exports = {
   reactStrictMode: true,

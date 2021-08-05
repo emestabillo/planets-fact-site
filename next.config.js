09265fe21cf3d87fdd7672d32d0607ad67a272fc
Emmilie Estabillo <emsestabillo@gmail.com>
2021-08-05 00:19:32 -0400

    Fix routing and imports

--- next.config.js
+++ next.config.js
@@ -3,13 +3,4 @@ module.exports = withImages();
 
 module.exports = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/Earth",
-        permanent: true,
-      },
-    ];
-  },
 };
--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,12 @@
-export default function Home() {}
+import { PlanetData } from "../data/planets";
+import Planet from "../pages/[planet]";
+
+const planet = PlanetData.find((planet) => planet.name === "Earth");
+
+export default function Home() {
+  return (
+    <>
+      <Planet planet={planet} />
+    </>
+  );
+}
